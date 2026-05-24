48e81a0f7f2592d4223e1bade3c04f345aee554c
Abdulsalam Idris <idrisbnabdussalam@gmail.com>
2026-05-24 00:09:22 -0700

    Updated the hero and nav

--- src/components/StatsSection.tsx
+++ src/components/StatsSection.tsx
@@ -4,7 +4,7 @@ export default function StatsSection() {
     return(
         <section className="px-6 py-16 bg-gray-100">
             <div className="max-w-6xl mx-auto">
-                <h2 className="text-3xl font-bold mb-10">Stact Analytics</h2>
+                <h2 className="text-3xl font-bold mb-10">Stack Analytics</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <StatCard 
                     title="Total Classes"
