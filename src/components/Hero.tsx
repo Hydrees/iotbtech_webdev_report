8225f60b31e5124ea2ddd8b32a4f2449a626d66f
Abdulsalam Idris <idrisbnabdussalam@gmail.com>
2026-05-23 23:52:43 -0700

    updated homepage

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,19 @@
 export default function Hero() {
     return (
-        <section className="bg-gradient-to-r from-green-700 to-yellow-400 text-white px-6 py-20">
-            <div className="max-w-6xl mx-auto">
-                <h1 className="text-4xl md:text-6xl font-bold loading-tight">Web Development <br/> Statutory Report Dashboard</h1>
-                <p className="mt-6 text-lg md:text-2xl max-w-3xl">Comprehensive visual representation of fellows' progress, perticipation, archievements and future projections within the Web Development Stack</p>
-                <button className="mt-8 bg-white text-green-700 font-semibold px-6 py-3 rounded-lg hover:bg-gray-300 transition">View Report</button>
+        <section className="relative overflow-hidden bg-gradient-to-br from-green-900 via-green-800 to-yellow-500 text-white px-6 py-15">
+            <div className="max-w-7xl mx-auto">
+                <div className="max-w-4xl">
+                    <p className="uppercase tracking-[6px] text-yellow-300 font-semibold mb-6">Web Dev Performance Report</p>
+                    <h1 className="text-5xl md:text-7xl font-black leading-tight">Web Development <br />  <span className="text-yellow-300">Dashboard Report</span></h1>
+                    <p className="mt-8 text-lg md:text-2xl text-gray-200 leading-9 max-w-3xl">Comprehensive visual representation of fellows' participation, performance, achievements, challenges and future projections within the Web Development Stack</p>
+
+                    {/* button */}
+                    <div className="flex items-center gap-5 mt-10">
+                        <a href="#overview" className="bg-yellow-400 text-black px-8 py-4 rounded-xl font-bold hover:scale-105 transition duration-300">View Report</a>
+                        <a href="#timeline" className="border border-white/30 px-8 py-4 rounded-xl font-semibold hover:bg-white hover:text-black transition duration-300">View Curricullum</a>
+                    </div>
+                </div>
             </div>
         </section>
     )
-}
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,32 +31,43 @@ const navItems = [
 ];
 
 export default function Navbar() {
-    const activeSection =useActiveSection();
+    const activeSection = useActiveSection();
 
     return (
-        <nav className="sticky top-0 z-50 bg-green-700 text-white px-6 py-4 flex items-center justify-between shadow-md">
-            <div className="flex items-center gap-3">
-                <Image
-                src="/IOTBTECH_LOGO.png"
-                alt="IOTBTECH Logo"
-                width={50}
-                height={0}
-                className="rounded-full object-cover"
-                />
-                {/* <h1 className="text-xl font-bold">IOTB-TECH WEBDEV REPORT</h1> */}
-            </div>
+        <nav className="sticky top-0 z-50 backdrop-blur-md bg-white/80 border-b border-gray-200">
+            <div className="max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
+                {/* left side */}
+                <div className="flex items-center gap-4">
+                    <Image
+                    src="/IOTBTECH_LOGO.png"
+                    alt="IOTB TECH LOGO"
+                    width={100}
+                    height={100}
+                    className="rounded-full object-cover"
+                     />
 
-            <ul className="hidden md:flex gap-6 font-medium">
-                {navItems.map((item) => (
-                    <li key={item.id}>
-                        <a href={`#${item.id}`}
-                        className={`transition ${activeSection === item.id ? "text-yellow-300" : "text-white"}`}
-                        >
-                            {item.name}
-                        </a>
-                    </li>
-                ))}
-            </ul>
+                     <div>
+                        {/* <h1 className="text-xl font-extrabold tracking-tight text-green-800">IOTBTECH</h1> */}
+                        <p className="text-sm text-gray-500 font-medium">Web Dev Report Dashboard</p>
+                     </div>
+                </div>
+
+                {/* nav links */}
+                <ul className="hidden lg:flex items-center gap-8">
+                    {navItems.map((item) => (
+                        <li key={item.id}>
+                            <a href={`#${item.id}`} className={`relative text-{15px} font-semibold transition
+                                 duration-300 hover:text-green-700 ${activeSection === item.id ? "text-green-700" : "text-gray-600" }`}> 
+                                {item.name}
+                                {activeSection === item.id && (
+                                    <span className="absolute left-0 -button-2 w-full h-[3px] rounded-full bg-yellow-400"></span>
+                                )}
+                                </a>
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
         </nav>
-    )
+    );
 }
